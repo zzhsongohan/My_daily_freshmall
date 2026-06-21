3ab3eb417b8786ccb66ab76397e26ad06f9e5f46
钟梓豪 <3529223875@qq.com>
2026-06-21 16:09:50 +0800

    初始化项目

---
+++ db/base_model.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class BaseModel(models.Model):
+    """模型抽象基类"""
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    update_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+    is_deleted = models.BooleanField(default=False, verbose_name="是否删除")
+
+    class Meta:
+        """说明是个抽象类"""
+        abstract = True
