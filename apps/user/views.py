7de24303ca2b21d595016443e858eb28726b4203
钟梓豪 <3529223875@qq.com>
2026-06-23 13:45:55 +0800

    Django升级，修改模板load导入

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+class RegisterView(View):
+    """注册类视图"""
+
+    def get(self, request):
+        """显示注册页面"""
+        return render(request, "register.html")
